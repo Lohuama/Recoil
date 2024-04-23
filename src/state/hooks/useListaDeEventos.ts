8370ef26d1763506538246732a0e1b610378da99
Lohuama <limalorruama98@gmail.com>
2024-04-23 12:37:45 -0300

    calendar

---
+++ src/state/hooks/useListaDeEventos.ts
@@ -0,0 +1,11 @@
+import { useRecoilValue } from "recoil";
+import { listaDeEventosState } from "../atom";
+
+const useListaDeEventos = () => {
+
+    return useRecoilValue(listaDeEventosState)
+    
+
+}
+
+export default useListaDeEventos;
