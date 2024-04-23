35460c5cf4d021695bec1ac32d216927e4e2126a
Lohuama <limalorruama98@gmail.com>
2024-04-23 12:46:43 -0300

    demonstrraçao

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -14,7 +14,7 @@ export const listaDeEventosState = atom<IEvento[]>({
         {
             "descricao": "Fernanda Lira",
             "inicio": new Date("2024-04-23T11:00"),
-            "fim": new Date("2024-04-23T11:30"),
+            "fim": new Date("2024-04-23T12:00"),
             "completo": false,
             "id": 1642342959,
         },
